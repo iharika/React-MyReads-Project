37fb28b9d7e5577dd5a961a0da0b27f0f6ed2026
Harika Chengelput <harika317@yahoo.com>
2018-12-04 14:49:53 +0000

    initial commit from master

--- src/App.js
+++ src/App.js
@@ -2,17 +2,44 @@ import React from 'react'
 import './App.css'
 import SearchBooks from './components/SearchBooks'
 import BooksList from './components/BooksList'
+import * as booksApi from './BooksAPI';
 
 class BooksApp extends React.Component {
-  state = {
-    /**
-     * TODO: Instead of using this state variable to keep track of which page
-     * we're on, use the URL in the browser's address bar. This will ensure that
-     * users can use the browser's back and forward buttons to navigate between
-     * pages, as well as provide a good URL they can bookmark and share.
-     */
-    showSearchPage: false
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      /**
+       * TODO: Instead of using this state variable to keep track of which page
+       * we're on, use the URL in the browser's address bar. This will ensure that
+       * users can use the browser's back and forward buttons to navigate between
+       * pages, as well as provide a good URL they can bookmark and share.
+       */
+      showSearchPage: false,
+      books: []
+
+    }
+    this.renderData = this.renderData.bind(this)
   }
+
+  componentDidMount() {
+    this.renderData()
+  }
+
+  renderData() {
+    booksApi.getAll().then(data => {
+      this.setState({
+        books: data
+      });
+    });
+  }
+
+  moveBookToShelf(bookToMove, shelfToMove) {
+    booksApi.update(bookToMove, shelfToMove).then(() => {
+      this.renderData()
+    });
+  }
+
 
   render() {
     return (
@@ -22,7 +49,7 @@ class BooksApp extends React.Component {
             <div className="list-books-title">
               <h1>MyReads</h1>
             </div>
-            <BooksList />
+            <BooksList books={this.state.books} onChange={this.moveBookToShelf} />
             <div className="open-search">
               <button onClick={() => this.setState({ showSearchPage: true })}>Add a book</button>
             </div>
