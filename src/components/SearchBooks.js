2312b5d2df45aa6e091b2a49533d996c3f87e8c5
Arun Kumar <harika317@yahoo.com>
2019-01-17 15:21:19 +0000

    Search result verifies user books shelves and displays existing ones

--- src/components/SearchBooks.js
+++ src/components/SearchBooks.js
@@ -7,13 +7,9 @@ import * as utils from '../utils/utils';
 
 class SearchBooks extends React.Component {
 	state = {
-		userBooks: this.props.userBooks,
 		Books: [],
 		searchValue: ''
 	};
-	componentDidMount() {
-		console.log(this.state.userBooks);
-	}
 	onSearch = event => {
 		if (event.target.value && event.target.value.length > 2) {
 			let value = event.target.value;
@@ -26,9 +22,24 @@ class SearchBooks extends React.Component {
 	addBookToShelf = (book, shelfToMove) => {
 		utils.moveBookToShelf(book, shelfToMove);
 	};
+	compareAndUpdateShelfForSearchResults(result) {
+		let userBooks = this.props.userBooks;
+		result.forEach(function(elem) {
+			elem.shelf = 'none';
+		});
+		for (let book of result) {
+			for (let userBook of userBooks) {
+				if (book.id === userBook.id) {
+					book.shelf = userBook.shelf;
+				}
+			}
+		}
+		return result;
+	}
 	searchBooks = value => {
 		booksApi.search(value).then(data => {
 			if (data.length > 0) {
+				data = this.compareAndUpdateShelfForSearchResults(data);
 				this.setState({ Books: data });
 			}
 		});
@@ -36,22 +47,22 @@ class SearchBooks extends React.Component {
 	render() {
 		const shelfBooks = this.state.Books;
 		return (
-			<div className="search-books">
-				<div className="search-books-bar">
-					<Link to="/">
-						<button className="close-search">Close</button>
+			<div className='search-books'>
+				<div className='search-books-bar'>
+					<Link to='/'>
+						<button className='close-search'>Close</button>
 					</Link>
-					<div className="search-books-input-wrapper">
+					<div className='search-books-input-wrapper'>
 						{}
 						<input
-							type="text"
-							placeholder="Search by title or author"
+							type='text'
+							placeholder='Search by title or author'
 							onChange={this.onSearch}
 						/>
 					</div>
 				</div>
-				<div className="search-books-results">
-					<ol className="books-grid">
+				<div className='search-books-results'>
+					<ol className='books-grid'>
 						{shelfBooks.map(data => (
 							<li key={data.id}>
 								<Book
